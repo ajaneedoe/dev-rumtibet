067a6d62924547ebbac22484df6e9f5f753d2216
Mac <mac@MacBook-Air-Mac.local>
2025-08-27 19:40:40 +0300

    feat: create input desktop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,17 @@
-  
+  let button = {
+    content: 'Select 2021-07-26',
+    className: 'custom-button-classname',
+    onClick: (dp) => {
+        let date = new Date('2021-07-26');
+        dp.selectDate(date);
+        dp.setViewDate(date);
+    }
+}
+
+new AirDatepicker('#calendar', {
+    buttons: [button, 'clear'], // Custom button, and pre-installed 'clear' button
+    position: 'top center'
+})
+
+NiceSelect.bind(document.getElementById("locations-select"), {searchable: true, placeholder: 'Локация для тура', searchtext: 'zoek', selectedtext: 'geselecteerd'});
+NiceSelect.bind(document.getElementById("members-select"), {searchable: false, placeholder: 'Участники', searchtext: 'zoek', selectedtext: 'geselecteerd'});
