061c56b4be580af5b608631b3f55c027c3fbbd9b
Mac <mac@MacBook-Air-Mac.local>
2025-09-05 12:36:52 +0300

    build: instal nice-select2

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,17 +1,52 @@
-  let button = {
-    content: 'Select 2021-07-26',
-    className: 'custom-button-classname',
+let button = {
+    content: "Select 2021-07-26",
+    className: "custom-button-classname",
     onClick: (dp) => {
-        let date = new Date('2021-07-26');
+        let date = new Date("2021-07-26");
         dp.selectDate(date);
         dp.setViewDate(date);
-    }
+    },
+};
+
+new AirDatepicker("#date", {
+    buttons: [button, "clear"], // Custom button, and pre-installed 'clear' button
+    position: "top center",
+});
+
+if (document.querySelector("select")) {
+    NiceSelect.bind(document.getElementById("locations-select"), {
+        searchable: true,
+        placeholder: "Локация для тура",
+        searchtext: "zoek",
+        selectedtext: "geselecteerd",
+    });
+    NiceSelect.bind(document.getElementById("members-select"), {
+        searchable: false,
+        placeholder: "Участники",
+        searchtext: "zoek",
+        selectedtext: "geselecteerd",
+    });
 }
 
-new AirDatepicker('#calendar', {
-    buttons: [button, 'clear'], // Custom button, and pre-installed 'clear' button
-    position: 'top center'
-})
+const forEach = function (t, o, r) {
+    if ("[object Object]" === Object.prototype.toString.call(t))
+        for (var c in t)
+            Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+    else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
+};
 
-NiceSelect.bind(document.getElementById("locations-select"), {searchable: true, placeholder: 'Локация для тура', searchtext: 'zoek', selectedtext: 'geselecteerd'});
-NiceSelect.bind(document.getElementById("members-select"), {searchable: false, placeholder: 'Участники', searchtext: 'zoek', selectedtext: 'geselecteerd'});
+const hamburgers = document.querySelectorAll(".hamburger");
+const burgerMenu = document.querySelector('.header__mobile-container');
+const body = document.querySelector('body');
+
+if (hamburgers.length > 0) {
+    forEach(hamburgers, function (hamburger) {
+        hamburger.addEventListener("click", function () {
+            this.classList.toggle("is-active");
+            burgerMenu.classList.toggle("show");
+            body.classList.toggle('overflow-hidden');
+        },
+            false
+        );
+    });
+}
