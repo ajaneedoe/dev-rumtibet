8c867d12450efd83fdf077b6778680af0e4ae27f
ajaneedoe <ajaneedoe@gmail.com>
2025-10-13 11:12:30 +0300

    request form for mobile

--- dev/assets/js/modal.js
+++ dev/assets/js/modal.js
@@ -1,40 +1,38 @@
-window.addEventListener("load", () => {
-    const initModal = () => {
+window.addEventListener('load', ()=> {
+    const initModal = ()=> {
         const trigerModalButton = document.querySelectorAll('[data-triger-modal]')
-        const ButtonCloseModal = document.querySelectorAll('[data-close-modal]')
+        const buttonCloseModal = document.querySelectorAll('[data-close-modal]')
         const modal = document.querySelectorAll('.modal');
 
-        const openModal = (modal) => {
+        const openModal = (modal)=> {
             modal.classList.add('show')
             body.classList.add('overflow-hidden');
         }
 
-        const closeModal = () => {
+        const closeModal = ()=> {
             const modal = document.querySelector('.modal.show')
-            modal.classList.remove('show');
+            modal.classList.remove('show')
             body.classList.remove('overflow-hidden');
         }
 
         //function open modal
         trigerModalButton.forEach(item => {
             const itemAttr = item.getAttribute('data-triger-modal')
-            item.addEventListener("click", () => {
+            item.addEventListener('click', ()=> {
                 const modal = document.getElementById(itemAttr)
                 openModal(modal)
             })
         })
 
         //function close modal
-        ButtonCloseModal.forEach(item => {
-            item.addEventListener("click", () => {
+        buttonCloseModal.forEach(item => {
+            item.addEventListener('click', ()=> {
                 closeModal()
             })
         })
-
         modal.forEach(item => {
-            item.addEventListener('click', (event) => event.target === item ? closeModal() : null);
+            item.addEventListener('click', (event)=>  event.target === item ? closeModal() : null);
         })
     }
-
     document.querySelector('[data-triger-modal]') ? initModal() : null
 })
--- dev/assets/js/request.js
+++ dev/assets/js/request.js
@@ -1,36 +1,110 @@
 const requestData = document.querySelector('#request-data')
-const locationSelect = document.querySelector('#location-select')
-const calendarInput = document.querySelector('#calendar')
-const memberSelect = document.querySelector('#member-select')
-const test = document.querySelector('#triger-request')
+const locationsSelect = document.querySelector('#locations-select')
+const dateInput = document.querySelector('#date')
+const membersSelect = document.querySelector('#members-select')
+const trigerRequest = document.querySelector('#triger-request')
 
 const requestConfig = {
     location: "",
     date: "",
-    member: ""
+    members: "",
 }
 
-locationSelect.addEventListener("change", () => {
-    let value = locationSelect.value
+locationsSelect.addEventListener('change', ()=> {
+    let value = locationsSelect.value
     requestConfig.location = value
-    console.log(requestConfig)
+    console.log(requestConfig);
 })
 
-calendarInput.addEventListener("change", () => {
-    let value = calendarInput.value
+dateInput.addEventListener('change', ()=> {
+    let value = dateInput.value
     requestConfig.date = value
-    console.log(requestConfig)
+    console.log(requestConfig);
 })
 
-memberSelect.addEventListener("change", () => {
-    let value = memberSelect.value
-    requestConfig.member = value
-    console.log(requestConfig)
+membersSelect.addEventListener('change', ()=> {
+    let value = membersSelect.value
+    requestConfig.members = value
+    console.log(requestConfig);
 })
 
-const setValue = () => {
-    const formValue = `Локация для тура: ${requestConfig.location}\nДата похода: ${requestConfig.date}\nУчастники: ${requestConfig.member}`
+const setValue = ()=> {
+    const formValue = `Локация для тура: ${requestConfig.location}\nДата похода: ${requestConfig.date}\nУчастники: ${requestConfig.members}`
     requestData.innerHTML = formValue
 }
 
-test.addEventListener("click", setValue)
+trigerRequest.addEventListener('click', setValue)
+
+const modalLocationsSelect = document.querySelector('#modal-locations-select')
+const modalDate = document.querySelector('#modal-date')
+const modalMembersSelect =  document.querySelector('#modal-members-select')
+const submitButton = document.querySelector('#submit-button')
+const message = document.querySelector('.message')
+const stepsNav = document.querySelector('.steps__nav')
+const stepsNavButtonFirst = document.querySelector('.steps__nav .btn_step-first')
+const stepsNavButtonSecond = document.querySelector('.steps__nav .btn_step-second')
+const layoutStepFirst = document.querySelector('.steps__first-step')
+const layoutStepSecond = document.querySelector('.steps__second-step')
+
+layoutStepSecond.classList.add('steps__second-step_hidden')
+
+const formConfig = {
+    emptyMessage: "",
+    errorMessage: "Все поля обязательны для заполнения. Заполните, пожалуйста, поля.",
+    validMessage: "Все поля заполнены. ✓",
+}
+
+const errorMessage = () => {
+    message.classList.remove('message__hidden')
+    message.classList.remove('message__valid')
+    message.innerHTML = formConfig.errorMessage
+}
+
+const removeValidMessage = () => {
+    message.classList.add('message__hidden')
+    message.innerHTML = formConfig.emptyMessage
+}
+
+const validMessage = () => {
+    message.classList.add('message__valid')
+    message.innerHTML = formConfig.validMessage
+    setTimeout(removeValidMessage, 2000);
+}
+
+const showNav =()=> {
+    stepsNav.classList.add('steps__nav_show')
+}
+
+const showStepFirst = ()=> {
+    layoutStepFirst.classList.remove('steps__first-step_hidden')
+    layoutStepSecond.classList.add('steps__second-step_hidden')
+}
+
+const showStepSecond = ()=> {
+    layoutStepFirst.classList.add('steps__first-step_hidden')
+    layoutStepSecond.classList.remove('steps__second-step_hidden')
+}
+
+stepsNavButtonFirst.addEventListener('click', showStepFirst)
+stepsNavButtonSecond.addEventListener('click', showStepSecond)
+
+const stepSecond = ()=> {
+    validMessage();
+    showNav();
+    showStepSecond();
+    console.log('step 2');
+}
+
+const checkedValue = ()=> {
+    if (modalLocationsSelect.value === "" || modalDate.value === "" || modalMembersSelect.value === "") {
+        errorMessage()
+    }
+    else {
+        stepSecond()
+    }
+}
+
+submitButton.addEventListener('click', checkedValue);
+
+
+
